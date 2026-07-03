efaa06660acec9a9bece4b27608ccb6ee6b7b98f
Luis Perez <luisperez2900@outlook.com>
2026-07-03 08:29:26 -0400

    feat: initial commit with nuxt and supabase setup

---
+++ nuxt.config.ts
@@ -0,0 +1,15 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  modules: ['@nuxtjs/supabase'],
+  supabase: {
+    redirect: false
+  },
+  runtimeConfig: {
+    public: {
+      supabaseUrl: process.env.SUPABASE_URL,
+      supabaseKey: process.env.SUPABASE_KEY
+    }
+  }
+})
